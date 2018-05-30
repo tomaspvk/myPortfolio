c4e18868db9fdd41f305f0195b6f3459d0ac8f04
Matúš Sedlák <sedlak87@gmail.com>
2018-05-30 19:32:57 +0200

    [impl] added application class

--- app/src/main/java/cz/muni/fi/xpavuk/myportfolio/activities/MainActivity.java
+++ app/src/main/java/cz/muni/fi/xpavuk/myportfolio/activities/MainActivity.java
@@ -30,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Realm.init(this);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
---
+++ app/src/main/java/cz/muni/fi/xpavuk/myportfolio/activities/PortolioApp.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.xpavuk.myportfolio.activities;
+
+import android.app.Application;
+import cz.muni.fi.xpavuk.myportfolio.R;
+import io.realm.Realm;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+public class PortolioApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/OpenSans-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+}
